135d127bbfea611b56a17f96e7e24f0ceb5ff8be
ccartix <grechiskina.kate@gmail.com>
2025-11-08 16:06:08 +0200

    Add files via upload

---
+++ LAB4/lab4.lisp
@@ -0,0 +1,69 @@
+(defun find-min (lst key test)
+  (if (null (cdr lst))
+      (car lst)
+      (let* ((head (car lst))
+             (head-key (funcall key head))
+             (tail-min (find-min (cdr lst) key test))
+             (tail-key (funcall key tail-min)))
+        (if (funcall test head-key tail-key)
+            head
+            tail-min))))
+
+(defun remove-one (element lst)
+  (cond ((null lst) nil)
+        ((equal element (car lst)) (cdr lst))
+        (t (cons (car lst) (remove-one element (cdr lst))))))
+
+(defun sort-func (lst &key (key #'identity) (test #'<))
+  (if (or (null lst) (null (cdr lst)))
+      lst
+      (let ((min (find-min lst key test)))
+        (cons min
+              (sort-func (remove-one min lst)
+                         :key key
+                         :test test)))))
+  
+(defun check-sort-func (name input expected &key (key #'identity) (test #'<))
+  (format t "~:[FAILED~;passed~]... ~a~%" 
+          (equal (sort-func input :key key :test test) expected) name))
+
+(defun test-sort-func ()
+  (check-sort-func "test 1" '(3 1 2) '(1 2 3))
+  (check-sort-func "test 2" nil nil)
+  (check-sort-func "test 3" '(5 5 1 3) '(1 3 5 5))
+  (check-sort-func "test 4" '(7) '(7))
+  (check-sort-func "test 6" '(-5 2 -3 8 -1 4) '(-1 2 -3 4 -5 8) :key #'abs)
+  (check-sort-func "test 7" '(1 2 3 4 5)   '(5 4 3 2 1)   :test #'>))
+
+
+(defun rpropagation-reducer (&key (comparator #'<))
+  (lambda (current acc)
+    (let* ((rightmost (car acc))
+           (best (if (and rightmost
+                          (not (funcall comparator current rightmost)))
+                     rightmost    
+                     current)))   
+      (cons best acc))))
+
+
+(defun check-rpropagation (name input expected &key (comparator #'<))
+  (let* ((result (reduce (rpropagation-reducer :comparator comparator)
+                         input
+                         :from-end t
+                         :initial-value nil)))
+    (format t "~:[FAILED~;passed~]... ~a~%"
+            (equal result expected)
+            name)))
+
+(defun test-rpropagation ()
+  (check-rpropagation "test 1" '(3 2 1 2 3) '(1 1 1 2 3))
+  (check-rpropagation "test 2" '(3 1 4 2) '(1 1 2 2))
+  (check-rpropagation "test 3" '(1 2 3) '(3 3 3) :comparator #'>))
+
+(defun run-tests ()
+  (format t "~%=== Functional Sort Tests ===~%")
+  (test-sort-func)
+  (format t "~%===  Tests rpropagation-reducer ===~%")
+  (test-rpropagation))
+
+(run-tests)
